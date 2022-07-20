4bb6dd8781549e554d8882b88a422bb275555826
Tetiana <skorochka666@gmail.com>
2022-07-20 12:39:50 +0300

    upd

--- serverMiddleware/redirects.js
+++ serverMiddleware/redirects.js
@@ -4,16 +4,9 @@ module.exports = function (req, res, next) {
   const env = "https://api.goldtowncompany.com";
   const forceDomain = "http://gt.org.ua";
 
-  if (host == "www.gt.org.ua" || host == "https://goldtowncompany.com") {
-    //   res.writeHead(301, { Location: forceDomain + url });
-    redirect(req, forceDomain + req.href);
-    // return res.end();
-  }
-
-  // if (req.headers.host == "www.old-site.net") {
-  //   redirect(req, "http://newsite.com" + req.href);
-  //   return;
-  // }
-
-  //   return next();
+  //   if (host == "www.gt.org.ua" || host == "https://goldtowncompany.com") {
+  //     //   res.writeHead(301, { Location: forceDomain + url });
+  //     redirect(req, forceDomain + req.href);
+  //     // return res.end();
+  //   }
 };
