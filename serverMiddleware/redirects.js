034c30b58244e202b110bcac496d8ae9858971da
Tetiana <skorochka666@gmail.com>
2022-07-20 12:29:23 +0300

    upd

--- serverMiddleware/redirects.js
+++ serverMiddleware/redirects.js
@@ -4,8 +4,11 @@ module.exports = function (req, res, next) {
   const env = "https://api.goldtowncompany.com";
   const forceDomain = "http://gt.org.ua";
 
+  console.log("host", host, url, env);
+
   if (env === "production" && host !== "www.gt.org.ua") {
     res.writeHead(301, { Location: forceDomain + url });
+    console.log("im hereeeeee");
     return res.end();
   }
 
