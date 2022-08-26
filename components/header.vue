8d476ccd0c0dacaf9347f647760548ac7dcd5eab
Tetiana <tetiana.kalinichenko@unicornpower.space>
2022-08-26 14:18:38 +0300

    upd

--- components/header.vue
+++ components/header.vue
@@ -7,11 +7,11 @@
             .header__list
                 //- p.header__item( @click="replase('about')") {{ $t('header.about_us') }}
                 n-link.header__item( :to="localePath('/purchase/all')" :class="{'show': show === 'purchase'}") {{ $t('header.purchase') }}
-                n-link.header__item(:class="{'show': show === 'construction'}" :to="localePath('/goldtownbud')") {{$t('header.bud')}}
+                //- n-link.header__item(:class="{'show': show === 'construction'}" :to="localePath('/goldtownbud')") {{$t('header.bud')}}
                 //- n-link.header__item( :to="localePath('/commerce/all')" :class="{'show': show === 'commerce'}") {{ $t('header.commerce') }}
                 //- p.header__item( @click="replase('request')") {{ $t('header.request') }}
                 p.header__item(@click="makeRequest") {{ $t('header.request') }}
-                n-link.header__item(:class="{'show': show === 'blog'}" :to="localePath('/blog')") Блог
+                //- n-link.header__item(:class="{'show': show === 'blog'}" :to="localePath('/blog')") Блог
                 n-link.header__item(:class="{'show': show === 'about_us'}" :to="localePath('/about_us')") {{ $t('header.about_us') }}
                 n-link.header__item(:class="{'show': show === 'contacts'}" :to="localePath('/contacts')") {{$t('header.contacts')}}
             .header__num-lang
